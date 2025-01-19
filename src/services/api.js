5e559c7676243a686afce7f9b4b1275ecc875be3
LytheanSem <lytheansem430@gmail.com>
2025-01-19 14:23:56 +0700

    modified API key

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+import { API_KEY, BASE_URL } from "../credential";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch popular movies");
+  }
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch search results");
+  }
+  const data = await response.json();
+  return data.results;
+};
